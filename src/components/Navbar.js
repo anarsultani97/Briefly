4c30e65274bfbd64c2f464995095e99a5f2ade4a
Anar Sutani <anar.s@code.edu.az>
2019-11-01 05:47:37 +0100

    Header is added

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import  '../style/style.css';
+
+
+const Navbar = props => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg py-3 navbar-light bg-light shadow-lg">
+                <div class="container">
+                    <a href="#" class="navbar-brand">
+                        <img src={props.img.imgURL}  width={'100px'} />
+                    </a>
+
+                    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                            class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+                    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active"><a href="#" class="nav-link">Home <span
+                                class="sr-only">(current)</span></a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+            );
+        };
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+// Importing React and ReactDOM
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Navbar from './components/Navbar'
+import logo from "./images/briefly-logo3.svg"
+import headerImg from "./images/homepage-bgpic.jpg"
+import Header from "./components/Header";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Creating a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const image  = {
+    imgURL: logo,
+    imgHeader: headerImg
+}
+const App = () => {
+    return <div>
+        <Navbar img={image}/>
+        <Header />
+    </div>
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
